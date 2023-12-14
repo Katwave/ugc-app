487bedaaa376b2061c67b85fc83f682bfbaf646b
Katwave <katlegokatwavebeats@gmail.com>
2023-12-14 03:35:26 +0200

    updated url for cors

--- api/config/cors.js
+++ api/config/cors.js
@@ -1,5 +1,5 @@
 const DEV_PORT = "http://localhost:3000" || "http://192.168.43.14:3000";
-const PROD_PORT = "https://ugc-app-client.vercel.app/";
+const PROD_PORT = "https://ugc-app-client.vercel.app";
 
 const cors = () => {
   if (process.env.NODE_ENV) {
